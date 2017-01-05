7bf4ac8b4a0af1afd4f684b857ac24b65b570b42
Harish Dhawan <hadhawan@microsoft.com>
2017-01-05 12:58:36 +0530

    Update index.js
    replaced year with minutes

--- routes/index.js
+++ routes/index.js
@@ -1,16 +1,16 @@
 "use strict";
 function index(req, res) {
-    res.render('index', { title: 'Express', year: new Date().getFullYear() });
+    res.render('index', { title: 'Express', year: new Date().getUTCMinutes() });
 }
 exports.index = index;
 ;
 function about(req, res) {
-    res.render('about', { title: 'About', year: new Date().getFullYear(), message: 'Your application description page' });
+    res.render('about', { title: 'About', year: new Date().getUTCMinutes(), message: 'Your application description page' });
 }
 exports.about = about;
 ;
 function contact(req, res) {
-    res.render('contact', { title: 'Contact', year: new Date().getFullYear(), message: 'Your contact page' });
+    res.render('contact', { title: 'Contact', year: new Date().getUTCMinutes(), message: 'Your contact page' });
 }
 exports.contact = contact;
 ;

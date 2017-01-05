5090c394b05dafc6fea21dba5e20514d41d498a0
Harish Dhawan <hadhawan@microsoft.com>
2017-01-05 12:42:06 +0530

    Update Gruntfile.js

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,6 +1,5 @@
 'use strict';
 
-var request = require('request');
 
 module.exports = function (grunt) {
   // show elapsed time at the end
@@ -57,20 +56,6 @@ module.exports = function (grunt) {
   files = grunt.config('watch.server.files');
   files = grunt.file.expand(files);
 
-  grunt.registerTask('delayed-livereload', 'Live reload after the node server has restarted.', function () {
-    var done = this.async();
-    setTimeout(function () {
-      request.get('http://localhost:' + reloadPort + '/changed?files=' + files.join(','),  function (err, res) {
-          var reloaded = !err && res.statusCode === 200;
-          if (reloaded) {
-            grunt.log.ok('Delayed live reload successful.');
-          } else {
-            grunt.log.error('Unable to make a delayed live reload.');
-          }
-          done(reloaded);
-        });
-    }, 500);
-  });
 
   grunt.registerTask('default', []);
 };
